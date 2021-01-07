0af4687b94a0f910581270d9f5b9600c2c64c9f2
Troy <troy-matthews@hotmail.com>
2021-01-07 20:48:43 +0000

    created navbar front-end that leads to different pages

---
+++ writerwhispers/src/components/create-story.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateStory extends Component {
+    render() {
+        return (
+            <div>
+                <p>you are in the create story component! </p>
+            </div>
+        )
+    }
+}
---
+++ writerwhispers/src/components/edit-story.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditStory extends Component {
+    render() {
+        return (
+            <div>
+                <p>you are in the edit story component! </p>
+            </div>
+        )
+    }
+}
